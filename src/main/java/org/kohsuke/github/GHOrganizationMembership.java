66054db379f4a857b1507654d1c0e98b22d1c6f7
cl4r1ty <paulambros@gmail.com>
2015-05-20 15:48:49 -0700

    Add in GHOrganizationMembership to check org membership type using the beta api Add new GHOrganizationMembership object Update Requester to allow for easily adding headers by returning self.

---
+++ src/main/java/org/kohsuke/github/GHOrganizationMembership.java
@@ -0,0 +1,44 @@
+/*
+ * © Copyright 2015 -  SourceClear Inc
+ */
+package org.kohsuke.github;
+
+import java.net.URL;
+
+/**
+ *
+ */
+public class GHOrganizationMembership {
+  private URL url, organization_url;
+
+  private String state, role;
+
+  private GHUser user;
+
+  private GHOrganization organization;
+
+  public URL getUrl() {
+    return url;
+  }
+
+  public URL getOrganizationUrl() {
+    return organization_url;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public GHUser getUser() {
+    return user;
+  }
+
+  public GHOrganization getOrganization() {
+    return organization;
+  }
+
+}
